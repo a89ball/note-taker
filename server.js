a818b1c521f1674300f0269a7b845f2c30801336
Abdifatah <abdifatah.101@outlook.com>
2021-10-27 12:01:45 +0100

    Set up routes

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+require("./routes/routes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log('server is running on port: ' + PORT)
+});
